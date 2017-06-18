5b350c8275d3efd098d5583f9746c2e62255e294
Viacheslav Krasnov <slavcheg21071993@gmail.com>
2017-06-18 21:13:10 +0300

    commit

---
+++ app/src/main/java/jdroidcoder/ua/taxi_bishkek/events/SimChangedEvent.java
@@ -0,0 +1,8 @@
+package jdroidcoder.ua.taxi_bishkek.events;
+
+/**
+ * Created by Maxim on 6/4/2017.
+ */
+
+public class SimChangedEvent {
+}
---
+++ app/src/main/java/jdroidcoder/ua/taxi_bishkek/events/UserCoordinateNullEvent.java
@@ -0,0 +1,8 @@
+package jdroidcoder.ua.taxi_bishkek.events;
+
+/**
+ * Created by Maxim on 6/3/2017.
+ */
+
+public class UserCoordinateNullEvent {
+}
